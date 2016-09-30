8921ce04647e3bfb29894e04ed7e40cbaf85d205
Stefan VanBuren <vanburenstefan@gmail.com>
2016-09-30 14:40:02 -0400

    Add prime and prime_test

---
+++ prime.go
@@ -0,0 +1,48 @@
+package main
+
+// IsPrime checks if an int is prime
+func IsPrime(n int) bool {
+	for b := Sqrt(n); b >= 1; b-- {
+		if b == 1 {
+			return true
+		}
+		if n%b == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// IsPrime64 checks if an int64 is prime
+func IsPrime64(n int64) bool {
+	for b := Sqrt64(n); b >= 1; b-- {
+		if b == 1 {
+			return true
+		}
+		if n%b == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// PrimeSieve implements the Sieve of Eratosthenes
+func PrimeSieve(len int) []int {
+	bools := make([]bool, len)
+	var primes []int
+	var i, count int
+	for i = 0; i < len; i++ {
+		bools[i] = true
+	}
+	// Sieve
+	for i = 2; i < len; i++ {
+		if bools[i] {
+			count++
+			primes = append(primes, i)
+			for k := 2; i*k < len; k++ {
+				bools[i*k] = false
+			}
+		}
+	}
+	return primes
+}
---
+++ prime_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestPrimeSieve(t *testing.T) {
+	x := PrimeSieve(1000)
+	for _, e := range x {
+		if !IsPrime(e) {
+			t.Errorf("Not prime element %d", e)
+		}
+	}
+}
