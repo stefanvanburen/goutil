2d30e17dc5026e7f8d065497830710913eb63e7c
Stefan VanBuren <vanburenstefan@gmail.com>
2016-10-04 18:14:23 -0400

    Use named returns in max and min

--- max.go
+++ max.go
@@ -3,24 +3,23 @@ package main
 import "math"
 
 // Max returns the maximum value of an int slice
-func Max(vals []int) int {
-	max := math.MinInt32
+func Max(vals []int) (max int) {
+	max = math.MinInt32
 	for _, v := range vals {
 		if v > max {
 			max = v
 		}
 	}
-	return max
+	return
 }
 
 // Max64 returns the maximum value of an int64 slice
-func Max64(vals []int64) int64 {
-	var max int64
-	max = math.MinInt64
+func Max64(vals []int64) (max int64) {
+	max = int64(math.MinInt64)
 	for _, v := range vals {
 		if v > max {
 			max = v
 		}
 	}
-	return max
+	return
 }
--- min.go
+++ min.go
@@ -3,24 +3,23 @@ package main
 import "math"
 
 // Min returns the minimum value of an int slice
-func Min(vals []int) int {
-	min := math.MaxInt32
+func Min(vals []int) (min int) {
+	min = math.MaxInt32
 	for _, v := range vals {
 		if v < min {
 			min = v
 		}
 	}
-	return min
+	return
 }
 
 // Min64 returns the minimum value of an int64 slice
-func Min64(vals []int64) int64 {
-	var min int64
-	min = math.MaxInt64
+func Min64(vals []int64) (min int64) {
+	min = int64(math.MaxInt64)
 	for _, v := range vals {
 		if v < min {
 			min = v
 		}
 	}
-	return min
+	return
 }
