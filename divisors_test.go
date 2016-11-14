2d00b8c2bcd220625e3e930f6921260d761758e9
Stefan VanBuren <vanburenstefan@gmail.com>
2016-11-14 11:03:51 -0500

    Fix prime_test and divisor_test

--- divisors_test.go
+++ divisors_test.go
@@ -8,14 +8,14 @@ import (
 
 var divisortests = []struct {
 	n        int
-	divisors int
+	divisors []int
 }{
 	{28, []int{1, 2, 4, 7, 14}},
 	{12, []int{1, 2, 3, 4, 6}},
 }
 
 func TestDivisors(t *testing.T) {
-	for _, x := range tests {
+	for _, x := range divisortests {
 		out := Divisors(x.n)
 		assert.Equal(t, out, x.divisors, "Should be equal")
 	}
--- prime_test.go
+++ prime_test.go
@@ -9,6 +9,6 @@ import (
 func TestPrimeSieve(t *testing.T) {
 	x := PrimeSieve(1000)
 	for _, e := range x {
-		assert.False(IsPrime(e), "Not prime element")
+		assert.True(t, IsPrime(e), "Not prime element")
 	}
 }
