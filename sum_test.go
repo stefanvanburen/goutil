aec736ca7625b7e8143f33436841167589f93574
Stefan VanBuren <vanburenstefan@gmail.com>
2019-07-23 11:26:37 -0400

    Rework sum tests, add more coverage

--- sum_test.go
+++ sum_test.go
@@ -6,16 +6,87 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSum(t *testing.T) {
-	var tests = []struct {
+func TestSumInt(t *testing.T) {
+	var tests = map[string]struct {
 		in  []int
 		out int
 	}{
-		{[]int{1, 2, 3}, 6},
-		{[]int{-1, -2, 3}, 0},
+		"positive": {[]int{1, 2, 3}, 6},
+		"mixed":    {[]int{-1, -2, 3}, 0},
+		"negative": {[]int{-1, -2, -3}, -6},
 	}
 
-	for _, x := range tests {
-		assert.Equal(t, SumInt(x.in), x.out, "Should be equal")
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, SumInt(tc.in), tc.out, "Should be equal")
+		})
+	}
+}
+
+func TestSumInt8(t *testing.T) {
+	var tests = map[string]struct {
+		in  []int8
+		out int8
+	}{
+		"positive": {[]int8{1, 2, 3}, 6},
+		"mixed":    {[]int8{-1, -2, 3}, 0},
+		"negative": {[]int8{-1, -2, -3}, -6},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, SumInt8(tc.in), tc.out, "Should be equal")
+		})
+	}
+}
+
+func TestSumInt16(t *testing.T) {
+	var tests = map[string]struct {
+		in  []int16
+		out int16
+	}{
+		"positive": {[]int16{1, 2, 3}, 6},
+		"mixed":    {[]int16{-1, -2, 3}, 0},
+		"negative": {[]int16{-1, -2, -3}, -6},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, SumInt16(tc.in), tc.out, "Should be equal")
+		})
+	}
+}
+
+func TestSumInt32(t *testing.T) {
+	var tests = map[string]struct {
+		in  []int32
+		out int32
+	}{
+		"positive": {[]int32{1, 2, 3}, 6},
+		"mixed":    {[]int32{-1, -2, 3}, 0},
+		"negative": {[]int32{-1, -2, -3}, -6},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, SumInt32(tc.in), tc.out, "Should be equal")
+		})
+	}
+}
+
+func TestSumInt64(t *testing.T) {
+	var tests = map[string]struct {
+		in  []int64
+		out int64
+	}{
+		"positive": {[]int64{1, 2, 3}, 6},
+		"mixed":    {[]int64{-1, -2, 3}, 0},
+		"negative": {[]int64{-1, -2, -3}, -6},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, SumInt64(tc.in), tc.out, "Should be equal")
+		})
 	}
 }
