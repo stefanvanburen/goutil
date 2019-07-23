86a45e4e89a9ee2e50e2b83eba2eb01037a4f794
Stefan VanBuren <vanburenstefan@gmail.com>
2019-07-23 11:44:43 -0400

    Add tests for Any, All, Filter, and Map

--- functional_test.go
+++ functional_test.go
@@ -41,3 +41,91 @@ func TestInclude(t *testing.T) {
 		})
 	}
 }
+
+func TestAny(t *testing.T) {
+	hasLength := func(s string) bool {
+		return len(s) > 0
+	}
+
+	var tests = map[string]struct {
+		haystack  []string
+		predicate func(string) bool
+		expected  bool
+	}{
+		"no matches": {[]string{"", ""}, hasLength, false},
+		"one match":  {[]string{"hi", ""}, hasLength, true},
+		"all match":  {[]string{"hi", "bye"}, hasLength, true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, Any(tc.haystack, tc.predicate), tc.expected)
+		})
+	}
+}
+
+func TestAll(t *testing.T) {
+	hasLength := func(s string) bool {
+		return len(s) > 0
+	}
+
+	var tests = map[string]struct {
+		haystack  []string
+		predicate func(string) bool
+		expected  bool
+	}{
+		"no matches": {[]string{"", ""}, hasLength, false},
+		"one match":  {[]string{"hi", ""}, hasLength, false},
+		"all match":  {[]string{"hi", "bye"}, hasLength, true},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, All(tc.haystack, tc.predicate), tc.expected)
+		})
+	}
+}
+
+func TestFilter(t *testing.T) {
+	hasLength := func(s string) bool {
+		return len(s) > 0
+	}
+
+	var tests = map[string]struct {
+		haystack  []string
+		predicate func(string) bool
+		expected  []string
+	}{
+		"no matches": {[]string{"", ""}, hasLength, nil},
+		"one match":  {[]string{"hi", ""}, hasLength, []string{"hi"}},
+		"all match":  {[]string{"hi", "bye"}, hasLength, []string{"hi", "bye"}},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, Filter(tc.haystack, tc.predicate), tc.expected)
+		})
+	}
+}
+
+func TestMap(t *testing.T) {
+	exclamation := func(s string) string {
+		return s + "!"
+	}
+
+	var tests = map[string]struct {
+		haystack  []string
+		transform func(string) string
+		expected  []string
+	}{
+		"no matches": {[]string{"", ""}, exclamation, []string{"!", "!"}},
+		"one match":  {[]string{"hi", ""}, exclamation, []string{"hi!", "!"}},
+		"all match":  {[]string{"hi", "bye"}, exclamation, []string{"hi!", "bye!"}},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, Map(tc.haystack, tc.transform), tc.expected)
+		})
+	}
+}
