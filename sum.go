8aa63741dfda4e45d651f42cbb124f369d18843e
Stefan VanBuren <stefan@vanburen.xyz>
2024-04-07 11:49:22 -0400

    Fix Sum doc comment

--- sum.go
+++ sum.go
@@ -2,11 +2,10 @@ package goutil
 
 import "golang.org/x/exp/constraints"
 
-// Sum returns the sum of `vals`.
+// Sum returns the sum of vals.
 func Sum[K constraints.Integer](vals ...K) (sum K) {
 	for _, v := range vals {
 		sum += v
 	}
-
 	return sum
 }
